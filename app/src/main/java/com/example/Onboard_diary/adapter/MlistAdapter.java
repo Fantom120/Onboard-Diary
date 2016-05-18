a69068b530301a8755d652710794a7388486b4c6
Ipro <i16p@yandex.ru>
2016-05-19 01:24:59 +0300

    impelemets onclickListener

---
+++ app/src/main/java/com/example/Onboard_diary/adapter/MlistAdapter.java
@@ -0,0 +1,76 @@
+package com.example.Onboard_diary.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.Onboard_diary.DataItem;
+import com.example.Onboard_diary.MainActivity;
+import com.example.Onboard_diary.R;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+
+
+public class MlistAdapter extends RecyclerView.Adapter<MlistAdapter.onBoardViewHolder>  {
+    private  List<DataItem> itemList;
+    private static DataItem item;
+    private View view;
+
+
+    private static SimpleDateFormat FORMATTER = new SimpleDateFormat("EE, dd MMMM yyyy", Locale.getDefault());
+
+
+    public MlistAdapter(List<DataItem> data_item){
+        this.itemList = data_item;
+    }
+
+    @Override
+    public onBoardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mitem, parent, false);
+
+        return new onBoardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(onBoardViewHolder holder, int position) {
+        item = itemList.get(position);
+        holder.personImageView.setImageResource(R.mipmap.ic_pen);
+        holder.nameTextView.setText(item.getTheme());
+        holder.description.setText(item.getDescription());
+        holder.date.setText(FORMATTER.format(item.getDate()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class onBoardViewHolder extends RecyclerView.ViewHolder {
+        public onBoardViewHolder(final View itemView) {
+            super(itemView);
+
+            CardView cardView = (CardView) itemView.findViewById(R.id.cardView);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MainActivity.onEditItem(item, "edit");
+                }
+            });
+
+        }
+
+        TextView nameTextView = (TextView) itemView.findViewById(R.id.textTheme);
+        TextView description = (TextView) itemView.findViewById(R.id.textDescription) ;
+        ImageView personImageView = (ImageView) itemView.findViewById(R.id.imageView);
+        TextView date = (TextView) itemView.findViewById(R.id.textDate);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/Onboard_diary/fragment/ItemDeleteDialogFragment.java
@@ -0,0 +1,66 @@
+package com.example.Onboard_diary.fragment;
+
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import com.example.Onboard_diary.DataItem;
+import com.example.Onboard_diary.Db_Main;
+import com.example.Onboard_diary.MainActivity;
+import com.example.Onboard_diary.R;
+import com.example.Onboard_diary.fragment.MainListFragment;
+
+public class ItemDeleteDialogFragment extends DialogFragment {
+    private Db_Main db;
+    private DataItem item;
+    private MainActivity activity;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        if (getActivity() != null) {
+            activity = (MainActivity) getActivity();
+            db = new Db_Main(activity);
+        }
+
+
+        if (getArguments() != null) {
+            item = getArguments().getParcelable("delete");
+        }
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.delete_item);  // заголовок
+        builder.setIcon(R.mipmap.delete);
+        builder.setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        db.deleteItem(item);
+                    }
+                }).run();
+                activity.onItemCreated(new MainListFragment());
+
+
+            }
+        });
+        builder.setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.setCancelable(true);
+
+        return builder.create();
+
+    }
+
+
+}
+
